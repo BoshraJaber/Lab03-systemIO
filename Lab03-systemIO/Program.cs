3800d8551f1007488735ac5299a4a64bb0a683e6
Boshra Husein Jaber <b.Jaber@ltuc.com>
2021-10-14 10:35:10 +0300

    Add project files.

---
+++ Lab03-systemIO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab03_systemIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
